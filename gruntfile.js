803c40e04bcaee9bd2b613d9d21b1ad8db90f43b
Josh Fabean <josh@codekoalas.com>
2015-04-25 16:27:00 -0500

    init commit, got basic things working

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    src: 'src',
+    dist: 'dist',
+
+    watch : {
+      grunt: {files: ['Gruntfile.js']},
+      postcss: {
+        files: 'src/*.css',
+        tasks: ['postcss']
+      }
+    },
+    
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('postcss-import')(),
+          require('postcss-simple-vars')(),
+          require('postcss-simple-extend')(),
+          require('postcss-nested')(),
+          require('postcss-media-minmax')(),
+          require('postcss-merge-rules')(),
+          require('autoprefixer-core')({browsers: 'last 2 version'}).postcss,
+          // require('csswring').postcss,
+        ]
+      },
+      dist: {
+        files: {
+          'dist/style.css': 'src/styles.css'
+        }
+      }
+    }
+
+
+  });
+
+
+
+  grunt.registerTask('default', ['postcss', 'watch']);
+
+};
